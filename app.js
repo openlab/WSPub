13059e8cba2873e3a8a45aa2fa9b4f1f4260b1c9
Nik Garkusha <nik@openlab.ca>
2014-01-17 11:02:00 -0800

    1st

---
+++ app.js
@@ -0,0 +1,36 @@
+var azure = require('azure');
+var program = require('commander');
+var config = require('./config');
+var WebSocket = require('ws');
+
+var sbService = azure.createServiceBusService(config.sbConnectionString);
+
+/*program
+    .version('0.0.1')
+    .option('-u, --url <url>')
+    .parse(process.argv);
+
+var ws = new WebSocket(program.url);*/
+
+var ws = new WebSocket(config.sourceSocket);
+
+ws.on('open', function() {
+    
+    console.log('connected');
+
+});
+
+ws.on('message', function(data, flags) {
+
+    console.log('receiving data...');
+    message = "{ body: " + data + " }";
+
+    console.log('sending message to service bus');
+    sbService.sendTopicMessage(config.sbTopic, message, function(error){
+        if(error) {
+            console.error(error);
+        }
+    });
+    console.log('message sent!');
+
+});
