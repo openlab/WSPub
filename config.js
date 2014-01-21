87bdbcc321257a11f3c48577c1253eeb4a331ed6
Nik Garkusha <nikg@live.ca>
2014-01-21 10:26:38 -0800

    Update config.js
    changed the sourceSocket to point to VM

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 ﻿module.exports = { 
-//	"sbFROMConnectionString":"Endpoint=sb://fanometer-test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=0NiCLDckAnr6pXwNcNWxFGrEXxtbP/7wPPQ8U2GYR3g=",
-//	"sbTOConnectionString":"Endpoint=sb://fanometer-test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=rq4EzPv8+26XfN6aVTp+4/dKhW6jdat2cDDQVwLdLNk=",
-//	"sbFROMTopic": "seackups",
-//	"sbTOTopic": "seahawks",
 	sbConnectionString: "Endpoint=sb://fanometer-test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=ZGyMU3i0gahbFiSafONR0up3iWbAGRAOQoS7SqGfoG8=",
         sbSubscription: "WebSocket",
 	sbTopic: "jmptest",
-	sourceSocket: "ws://products01.ess.washington.edu:16019"
+	sourceSocket: "ws://fanometer-test.cloudapp.net:9999"
 };
